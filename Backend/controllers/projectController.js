520e9f2c6f864768ee5057a29f289b8438e22b5f
EinoHaikonen <eahaik@utu.fi>
2026-03-22 18:54:13 +0200

    user input validation and updated documentation

--- Backend/controllers/projectController.js
+++ Backend/controllers/projectController.js
@@ -2,6 +2,7 @@ import {
   getProjectData,
   getProjects,
 } from "../repositories/projectRepository.js";
+import Joi from "joi";
 
 // for Frontend to retrieve project names and id
 export const readAll = async (req, res) => {
@@ -14,10 +15,24 @@ export const readAll = async (req, res) => {
   }
 };
 
+// input validation
+const inputValidation = Joi.object({
+  projectId: Joi.number().integer().required(),
+});
+
 // for Frontend to retrieve lagging indicators
 export const readData = async (req, res) => {
+  const { error, value } = inputValidation.validate(req.query, {
+    abortEarly: false,
+  });
+
+  if (error) {
+    const errorMessage = error.details[0].message;
+    return res.status(400).json({ error: errorMessage });
+  }
+
   try {
-    const projectData = await getProjectData(req);
+    const projectData = await getProjectData(value);
     return res.status(200).json(projectData);
   } catch (error) {
     console.log(error);
